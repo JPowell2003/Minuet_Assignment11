a297df16609a96d53ce17511c18154de6730d5b0
JPowell2003 <75809385+JPowell2003@users.noreply.github.com>
2025-04-14 20:22:06 -0400

    Added class structure for the FileWriter with methodd

--- Minuet_Assignment11/Output_Package/file_writer.py
+++ Minuet_Assignment11/Output_Package/file_writer.py
@@ -3,3 +3,32 @@
 # Write the Pepsi rows
 # Logs how many records were written and how many were cleaned
 # Ensure columns match original format
+
+class FileWriter:
+    def __init__(self, cleaned_df, anomalies_df):
+        '''
+        Store the cleaned and anomaly data for writing
+        '''
+
+    def write_cleaned_data(self, filepath):
+        '''
+        Writes the cleaned fuel data to 'cleanedData.csv'
+        Logs how many rows were written
+        '''
+
+    def write_anomalies(self, filepath):
+        '''
+        Writes the Pepsi anomalies to 'dataAnomalies.csv'
+        Logs how many rows were written
+        '''
+
+    def get_row_counts(self):
+        '''
+        Returns a dictionary or tuple with counts of total cleaned rows and total pepsi rows
+        '''
+
+    def validate_columns(self, reference_columns):
+        '''
+        Ensures that the cleaned and anomaly data use the same columns
+        as the original input file. Reorders or fills columns if necessary
+        '''
